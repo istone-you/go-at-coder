8c0132ec183693616762f6ac21c4ca0fd7854862
istone-you <istoneyou1205@yahoo.co.jp>
2023-08-18 13:56:49 +0900

    ABC311/A

--- ABC311/A - First ABC/main.go
+++ ABC311/A - First ABC/main.go
@@ -7,22 +7,19 @@ import (
 func main() {
 	var n int
 	var s string
-
-	fmt.Scan(&n)
-	fmt.Scan(&s)
+	fmt.Scan(&n, &s)
 
 	var a, b, c bool
 
 	for i := 0; i < n; i++ {
 		if s[i] == 'A' {
 			a = true
-		}
-		if s[i] == 'B' {
+		} else if s[i] == 'B' {
 			b = true
-		}
-		if s[i] == 'C' {
+		} else if s[i] == 'C' {
 			c = true
 		}
+
 		if a && b && c {
 			fmt.Println(i + 1)
 			break
